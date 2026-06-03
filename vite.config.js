f50d04e43187e09628214a7983d1ce7604fb5352
Franco Fantomius <francofantomius@gmail.com>
2026-06-03 12:19:34 +0200

    Move to Github Pages

--- vite.config.js
+++ vite.config.js
@@ -7,6 +7,7 @@ import { fileURLToPath } from 'url';
 const __dirname = path.dirname(fileURLToPath(import.meta.url));
 
 export default defineConfig({
+  base: './',
   resolve: {
     alias: {
       'fs-extra': path.resolve(__dirname, 'js/mocks/fs-extra.js'),
